a645ab33059317beabe79a1a219778f53f742cf9
blackparadise0407 <blackparadise0407@gmail.com>
2022-01-01 02:14:06 +0700

    fix database error

--- app/server/config/database.py
+++ app/server/config/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb://localhost:27017/")
 
 database = client.rsa
 
-user_collection = database.get_collection("users_collection")
+user_collection = database.get_collection("users")
