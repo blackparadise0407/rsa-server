577d918362669c27ad1e4f803ac4ae95e80fe0df
blackparadise0407 <blackparadise0407@gmail.com>
2022-01-01 21:16:54 +0700

    add catch exception middlewares

--- app/server/app.py
+++ app/server/app.py
@@ -1,12 +1,35 @@
-from fastapi import FastAPI
+import traceback
 
-from .routes.user import router as UserRouter
-from .routes.auth import router as AuthRouter
+from fastapi import FastAPI, Form
+from fastapi.param_functions import Depends
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from starlette.requests import Request
+from starlette.responses import Response
+
+from .routes.auth_route import router as AuthRouter
+from .routes.user_route import router as UserRouter
+from .services.auth_service import jwt_authentication
 
 app = FastAPI()
 
-app.include_router(UserRouter, tags=["User"], prefix="/user")
+app.include_router(
+    UserRouter,
+    tags=["User"],
+    prefix="/user",
+    dependencies=[Depends(jwt_authentication)],
+)
 app.include_router(AuthRouter, tags=["Auth"], prefix="/auth")
+
+
+async def catch_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception:
+        traceback.print_exc()
+        return Response("Internal server error", status_code=500)
+
+
+app.middleware("http")(catch_exceptions_middleware)
 
 
 @app.get("/", tags=["Root"])
